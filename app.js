4fbc7e5b61d25cb22ccef08f198a4b401edd6e8d
peelarajkumar <peelarajkumar@gmail.com>
2024-07-28 18:24:03 +0530

    Added Parcel

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const header = React.createElement("h1",{"id":"root","value":"Raj"},"My Name is Raj")
-const childers = React.createElement("div",{"id":"parentdiv"},[React.createElement("h1",{"id":"child1"},"First Child"),React.createElement("h1",{"id":"child2"},"Second Child"),React.createElement("h1",{"id":"child3"},"Third Child")]);
+import React from "react";
+import ReactDOM from "react-dom/client" 
+
+
+const header = React.createElement("h1",{"id":"root","value":"Raj"},"My Name is Raj Kumar !!")
+const childers = React.createElement("div",{"id":"parentdiv"},[React.createElement("h1",{"id":"child1"},"First Child is here"),React.createElement("h1",{"id":"child2"},"Second Child"),React.createElement("h1",{"id":"child3"},"Third Child")]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(childers);
